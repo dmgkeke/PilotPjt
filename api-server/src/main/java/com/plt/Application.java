d2be254a2dcf0493b2565f5b1f2c107712205259
오승철 <sch.oh@samsung.com>
2016-11-17 20:04:25 +0900

    build file update

--- api-server/src/main/java/com/plt/Application.java
+++ api-server/src/main/java/com/plt/Application.java
@@ -1,8 +1,11 @@
 package com.plt;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
 @EnableAutoConfiguration
 public class Application {
-	public static void main(String[] arg) {
+	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 }
