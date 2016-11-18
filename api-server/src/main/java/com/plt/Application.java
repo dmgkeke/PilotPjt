124313c33bfe948dfb0a02d02da470d1d4694edd
오승철 <sch.oh@samsung.com>
2016-11-18 17:30:51 +0900

    Application scan 수정

--- api-server/src/main/java/com/plt/Application.java
+++ api-server/src/main/java/com/plt/Application.java
@@ -2,8 +2,10 @@ package com.plt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @EnableAutoConfiguration
+@ComponentScan({"com.plt.rest", "com.plt.db.hsql"})
 public class Application {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
